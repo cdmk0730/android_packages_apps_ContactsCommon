dc68158ab085479074e31acf489748c698bd03eb
martincz <martincz@mokeedev.com>
2014-06-10 21:37:32 +0800

    Code Cleanup
    Change-Id: I030fe259de8697715f1a5d40fb2cf4fbf01c7e17

--- Android.mk
+++ Android.mk
@@ -27,6 +27,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-common \
     android-support-v13 \
     android-support-v4 \
+    libMoKeePinYin \
 
 LOCAL_PACKAGE_NAME := com.android.contacts.common
 
--- src/com/android/contacts/common/lettertiles/LetterTileDrawable.java
+++ src/com/android/contacts/common/lettertiles/LetterTileDrawable.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2014 The MoKee OpenSource Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +16,8 @@
  */
 
 package com.android.contacts.common.lettertiles;
+
+import org.chinese.pinyin.PinyinHelper;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
@@ -154,6 +157,24 @@ public class LetterTileDrawable extends Drawable {
             // offset
             canvas.drawText(sFirstChar, 0, 1, bounds.centerX(),
                     bounds.centerY() + mOffset * bounds.height() + sRect.height() / 2,
+                    sPaint);
+        } else if (mDisplayName != null && PinyinHelper.matchesCheck(mDisplayName.charAt(0))) {
+            // Draw letter/digit only if the first character is a chinese letter
+            // Draw letter or digit.
+            sFirstChar[0] = Character.toUpperCase(mDisplayName.charAt(0));
+
+            // Scale text by canvas bounds and user selected scaling factor
+            final int minDimension = Math.min(getBounds().width(), getBounds().height());
+            sPaint.setTextSize(mScale * sLetterToTileRatio * minDimension * 0.8f);
+            //sPaint.setTextSize(sTileLetterFontSize);
+            sPaint.getTextBounds(sFirstChar, 0, 1, sRect);
+            sPaint.setColor(sTileFontColor);
+            final Rect bounds = getBounds();
+
+            // Draw the letter in the canvas, vertically shifted up or down by the user-defined
+            // offset
+            canvas.drawText(sFirstChar, 0, 1, bounds.centerX(),
+                    bounds.centerY() * 0.9f + mOffset * bounds.height() + sRect.height() / 2,
                     sPaint);
         } else {
             // Draw the default image if there is no letter/digit to be drawn
